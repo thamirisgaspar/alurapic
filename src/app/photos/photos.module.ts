a7948abeb1b3014ed377cb51908cb746985f51bf
thamirisgaspar <thamiris_cristina_962@hotmail.com>
2020-09-04 15:47:03 -0300

    Modulo 1 Aula 3

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
     declarations : [
        PhotoComponent 
+    ],
+    imports : [
+        HttpClientModule
     ],
     exports : [
         PhotoComponent
