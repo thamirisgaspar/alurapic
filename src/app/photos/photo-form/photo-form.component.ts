0a02dc1a88519dcc0a5eea2f99776151ea43e0c2
thamirisgaspar <thamiris_cristina_962@hotmail.com>
2020-09-08 14:08:37 -0300

    Modulo 1 Aula 4

---
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ap-photo-form',
+  templateUrl: './photo-form.component.html'
+})
+export class PhotoFormComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {}
+}
