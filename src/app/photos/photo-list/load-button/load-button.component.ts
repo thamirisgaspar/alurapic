ddc08aee0ac9ae239e52caac0da388b119e21d55
thamirisgaspar <thamiris_cristina_962@hotmail.com>
2020-09-21 14:49:51 -0300

    Modulo 1 Aula 6

---
+++ src/app/photos/photo-list/load-button/load-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'ap-load-button',
+  templateUrl: './load-button.component.html'
+})
+export class LoadButtonComponent implements OnInit {
+  @Input() hasMore: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {}
+}
